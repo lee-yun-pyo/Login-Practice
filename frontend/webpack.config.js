d0fc95cb240c256bcbf5fc06a814416fff015444
이윤표 <yun2193@hanmail.net>
2024-04-23 02:13:32 +0900

    #fe config: 개발환경구축(webpack 이용)

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  // 번들링된 js 파일 이름과 저장될 경로 지정
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src/js")],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  mode: "development",
+};
