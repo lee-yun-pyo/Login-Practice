fb52f529cab1f53eeb81722c7522e6edd072f8e1
이윤표 <yun2193@hanmail.net>
2024-04-25 23:51:38 +0900

    #fe feat: 채팅창 UI 구현

---
+++ frontend/src/view/chatting.js
@@ -0,0 +1,54 @@
+const $chattingDiv = document.createElement("div");
+$chattingDiv.classList.add("chatting-container");
+
+function Title() {
+  const $title = document.createElement("h2");
+  $title.innerText = "게시판";
+  return $title;
+}
+
+function Board() {
+  const $board = document.createElement("div");
+  $board.classList.add("chatting-board");
+  return $board;
+}
+
+function MessageInput() {
+  const isLogin = false; // Todo: 로그인 상태 처리
+
+  const $box = document.createElement("div");
+  $box.classList.add("chatting-inputBox");
+
+  const $input = document.createElement("input");
+  $input.setAttribute("type", "text");
+  $input.setAttribute("placeholder", "내용을 입력하세요");
+  $input.classList.add("messageInput-input");
+
+  const $button = document.createElement("input");
+  $button.setAttribute("type", "submit");
+  $button.setAttribute("value", "전송");
+  $button.classList.add("messageInput-button");
+
+  const $loginBtnBox = document.createElement("div");
+  $loginBtnBox.classList.add("chatting-loginBtnBox");
+  $loginBtnBox.innerHTML = `
+    <span>채팅하기 위해 <a href="/frontend/login">로그인</a> 해주세요.<span>
+  `;
+
+  isLogin ? $box.append($input, $button) : $box.append($loginBtnBox);
+
+  return $box;
+}
+
+function render() {
+  $chattingDiv.appendChild(Title());
+  const $chatBox = document.createElement("div");
+  $chatBox.classList.add("chatting-box");
+  $chatBox.appendChild(Board());
+  $chatBox.appendChild(MessageInput());
+  $chattingDiv.appendChild($chatBox);
+}
+
+render();
+
+export default $chattingDiv;
