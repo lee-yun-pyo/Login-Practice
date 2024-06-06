f75debebc657bcb7c648dddbea2b2c8b57c4c9cb
이윤표 <yun2193@hanmail.net>
2024-06-07 03:47:17 +0900

    #fe docs: comment 스토어 지속 리덕스에서 일반 리덕스로 변경

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,85 +2,17 @@ import { combineReducers, legacy_createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import { COMMENT_ACTION_TYPES, USER_ACTION_TYPES } from "../constants";
+import { commentReducer, userReducer } from "./reducer";
 
-const persistConfig = {
-  key: "root",
+const userPersistConfig = {
+  key: "user",
   storage,
 };
 
-const USER_INIT_STATE = {
-  isLogin: false,
-  username: "",
-  userId: "",
-  isLoading: false,
-  expiresAt: null,
-};
-
-const POST_INIT_STATE = {
-  comments: [],
-};
-
-function userReducer(state = USER_INIT_STATE, action) {
-  switch (action.type) {
-    case USER_ACTION_TYPES.LOGIN:
-      return {
-        ...state,
-        isLogin: action.isLogin,
-        userId: action.userId,
-        username: action.username,
-        isLoading: false,
-        expiresAt: action.expiresAt,
-      };
-    case USER_ACTION_TYPES.INIT_LOADED:
-      return {
-        ...state,
-        isLogin: action.isLogin,
-        userId: action.userId,
-        username: action.username,
-        isLoading: false,
-      };
-    case USER_ACTION_TYPES.LOGOUT:
-      return USER_INIT_STATE;
-    case "LOADING":
-      return {
-        ...state,
-        isLoading: true,
-      };
-    default:
-      return state;
-  }
-}
-
-function commentReducer(state = POST_INIT_STATE, action) {
-  switch (action.type) {
-    case COMMENT_ACTION_TYPES.SET_COMMENTS:
-      return {
-        ...state,
-        comments: [...action.fetchedComments],
-      };
-    case COMMENT_ACTION_TYPES.ADD_COMMENT:
-      return {
-        ...state,
-        comments: [...state.comments, action.newComment],
-      };
-    case COMMENT_ACTION_TYPES.DELETE_COMMENT:
-      return {
-        ...state,
-        comments: state.comments.filter(
-          (comment) => comment._id !== action.commentId
-        ),
-      };
-    default:
-      return state;
-  }
-}
-
 const rootReducer = combineReducers({
-  user: userReducer,
+  user: persistReducer(userPersistConfig, userReducer),
   comment: commentReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = legacy_createStore(persistedReducer);
+export const store = legacy_createStore(rootReducer);
 export const persistor = persistStore(store);
